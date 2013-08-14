caacd681121eaf9c5fd66f1284d9956cb8c6a51e
Kjetil Valle <kjetil.valle@gmail.com>
2013-08-14 20:00:10 +0200

    refactored + removed hardcoded cakes

--- cake.elm
+++ cake.elm
@@ -1,17 +1,18 @@
 import Window
 import Mouse
 
-display (w,h) (x,y) = 
-    let cake posx posy = 
-            image 500 573 "/cake.gif" 
-                |> width 45 
-                |> toForm 
-                |> move (toFloat posx - toFloat w / 2, toFloat h / 2 - toFloat posy)
-        cakes = [ cake (w `div` 2) (h `div` 2)
-                , cake (w `div` 3) (h `div` 4)
-                , cake x y]
-        drawCakes = collage w h cakes
-    in drawCakes
+clickPos =
+    sampleOn Mouse.clicks Mouse.position
 
-main = 
-    lift2 display Window.dimensions Mouse.position
+makeCake w h x y = 
+    image 500 573 "/cake.gif" 
+        |> width 45 
+        |> toForm 
+        |> move (toFloat x - toFloat w / 2, toFloat h / 2 - toFloat y)
+
+display (w,h) (x,y) = 
+    let cake = makeCake w h
+    in  collage w h [cake x y]
+
+main =
+    display <~ Window.dimensions ~ clickPos
