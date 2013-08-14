dbe6b38e96bd99b98e043e036b7b5d6dbd57cc4c
Kjetil Valle <kjetil.valle@gmail.com>
2013-08-14 08:12:08 +0200

    cake at cursor

--- cake.elm
+++ cake.elm
@@ -2,10 +2,16 @@ import Window
 import Mouse
 
 display (w,h) (x,y) = 
-    let cake posx posy = image 500 573 "/cake.gif" |> width 45 |> toForm 
-        |> move (toFloat posx - toFloat w / 2, toFloat h / 2 - toFloat posy)
-    in
-        collage w h [ cake (w `div` 2) (h `div` 2)
-                    , cake (w `div` 3) (h `div` 4)]
+    let cake posx posy = 
+            image 500 573 "/cake.gif" 
+                |> width 45 
+                |> toForm 
+                |> move (toFloat posx - toFloat w / 2, toFloat h / 2 - toFloat posy)
+        cakes = [ cake (w `div` 2) (h `div` 2)
+                , cake (w `div` 3) (h `div` 4)
+                , cake x y]
+        drawCakes = collage w h cakes
+    in drawCakes
 
-main = lift2 display Window.dimensions Mouse.position
+main = 
+    lift2 display Window.dimensions Mouse.position
