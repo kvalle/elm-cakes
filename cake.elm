5c9c0e85f39ee6706b57a02fdcba02b6927713d1
Kjetil Valle <kjetil.valle@gmail.com>
2013-08-16 22:13:54 +0200

    refactoring

--- cake.elm
+++ cake.elm
@@ -6,57 +6,56 @@ import Random
 
 (gameWidth, gameHeight) = (800, 500)
 maxCakes = 10
+cakeSize = 45
 
 type Pos = (Int, Int)
 type State = { cakes:[Pos] }
 
 data Input pos = Click pos | Rand pos
 
--- SIGNALS
+-- INPUTS
 
-randomPos : Signal Pos
-randomPos = 
+input : Signal (Input Pos)
+input = 
     let tick = (every second)
         randX = (Random.range 20 gameWidth tick)
         randY = (Random.range 20 gameHeight tick)
-    in lift2 (,) randX randY
-
-input : Signal (Input (Int, Int))
-input = 
-    let clicks = (\pos -> Click pos) <~ sampleOn Mouse.clicks Mouse.position
-        randoms = (\pos -> Rand pos) <~ randomPos
+        clicks = Click <~ sampleOn Mouse.clicks Mouse.position
+        randoms = lift Rand <| (,) <~ randX ~ randY
     in merge clicks randoms
+
+-- GAME UPDATE
+
+initialState : State
+initialState = {cakes=[]}
 
 isNotTooClose : Pos -> Pos -> Bool
 isNotTooClose p1 p2 =
     let dist (x,y) (x',y') = sqrt <| (x-x')^2 + (y-y')^2
         distance = dist p1 p2
-    in distance > 45
+    in distance > cakeSize
 
 update : Input Pos -> State -> State
-update input oldState = 
+update input state = 
     case input of 
-        Click pos -> { oldState | cakes <- filter (isNotTooClose pos) oldState.cakes }
-        Rand pos -> if length oldState.cakes < maxCakes 
-                    then { oldState | cakes <- pos::oldState.cakes }
-                    else oldState
+        Click pos -> { state | cakes <- filter (isNotTooClose pos) state.cakes }
+        Rand pos -> if length state.cakes < maxCakes 
+                    then { state | cakes <- pos::state.cakes }
+                    else state
             
-currentCakes : Signal State
-currentCakes = foldp update {cakes=[]} input
+-- DISPLAY
 
--- DRAWING
-
-makeCakeAt w h (x, y) = 
-    image 500 573 "/cake.gif" 
-        |> width 45 
-        |> toForm 
-        |> move (toFloat x - toFloat w / 2, toFloat h / 2 - toFloat y)
-
+display : Pos -> State -> Element
 display (w,h) {cakes} = 
-    let makeCake = makeCakeAt w h
-    in  collage w h <| map makeCake cakes
+    let makeCake (x, y) = 
+            image 500 573 "/cake.gif" 
+                |> width cakeSize 
+                |> toForm 
+                |> move (toFloat x - toFloat w / 2, toFloat h / 2 - toFloat y)
+    in collage w h <| map makeCake cakes
 
 -- MAIN
 
 main =
-    display <~ Window.dimensions ~ currentCakes
+    let state = foldp update initialState input
+    in  display <~ Window.dimensions ~ state
